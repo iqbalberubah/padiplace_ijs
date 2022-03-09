3d004e711349addbb86fd6287402b0a514d6b8b1
Iqbal <iqbal.berubah@gmail.com>
2022-03-09 11:26:51 +0700

    update

--- handler/peternak.go
+++ handler/peternak.go
@@ -2,18 +2,22 @@ package handler
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	d "padiplace_ijs/database"
 	e "padiplace_ijs/entity"
-
-	"github.com/gorilla/mux"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var user e.Peternak
-	params := mux.Vars(r)
-	if result := d.DB.Table("peternak").Where("tlp_peternak = ? AND password1_user = ?", params["tlp_peternak"], params["password1_user"]).First(&user); result.Error != nil {
+	var body map[string]interface{}
+	buffer, _ := ioutil.ReadAll(r.Body)
+	if err := json.Unmarshal(buffer, &body); err != nil {
+		json.NewEncoder(w).Encode(e.ErrorResponse{404, "Request body tidak sesuai"})
+		return
+	}
+	if result := d.DB.Table("peternak").Where("tlp_peternak = ? AND password1_user = ?", body["tlp_peternak"], body["password1_user"]).First(&user); result.Error != nil {
 		json.NewEncoder(w).Encode(e.ErrorResponse{404, "User tidak ditemukan"})
 	} else {
 		json.NewEncoder(w).Encode(e.SuccesResponse{0, "Succes", user})
--- handler/product.go
+++ handler/product.go
@@ -19,36 +19,36 @@ func GetProducts(w http.ResponseWriter, r *http.Request) {
 func GetProduct(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	var products e.Product
-	if result := d.DB.Table("product").First(&products, params["id"]); result.Error != nil {
+	var product e.Product
+	if result := d.DB.Table("product").First(&product, params["id"]); result.Error != nil {
 		json.NewEncoder(w).Encode(e.ErrorResponse{404, "Product tidak ditemukan"})
 	} else {
-		json.NewEncoder(w).Encode(e.SuccesResponse{0, "Succes", products})
+		json.NewEncoder(w).Encode(e.SuccesResponse{0, "Succes", product})
 	}
 }
 
 func CreateProduct(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var products e.Product
-	json.NewDecoder(r.Body).Decode(&products)
-	d.DB.Table("product").Create(&products)
-	json.NewEncoder(w).Encode(products)
+	var product e.Product
+	json.NewDecoder(r.Body).Decode(&product)
+	d.DB.Table("product").Create(&product)
+	json.NewEncoder(w).Encode(product)
 }
 
 func UpdateProduct(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	var products e.Product
-	d.DB.Table("product").First(&products, params["id"])
-	json.NewDecoder(r.Body).Decode(&products)
-	d.DB.Table("product").Save(&products)
-	json.NewEncoder(w).Encode(products)
+	var product e.Product
+	d.DB.Table("product").First(&product, params["id"])
+	json.NewDecoder(r.Body).Decode(&product)
+	d.DB.Table("product").Save(&product)
+	json.NewEncoder(w).Encode(product)
 }
 
 func DeleteProduct(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	var products e.Product
-	d.DB.Table("product").Delete(&products, params["id"])
-	json.NewEncoder(w).Encode(products)
+	var product e.Product
+	d.DB.Table("product").Delete(&product, params["id"])
+	json.NewEncoder(w).Encode(product)
 }
