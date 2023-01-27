83059286bd965c15117cfb0894b01dbbaa26e54f
iqbal.berubah <iqbal.berubah@gmail.com>
2023-01-27 16:22:24 +0700

    update

--- handler/user.go
+++ handler/user.go
@@ -20,14 +20,18 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(e.ErrorResponse{404, "Request body tidak sesuai"})
 		return
 	}
-	if result := d.DB.Table("user").Where("username = ? OR tlp_user = ?", body["username"], body["tlp_user"]).First(&user); result.Error != nil {
+	if result := d.DB.Table("user").Where("username = ? OR tlp_user = ? OR email_user = ?", body["username"], body["username"], body["username"]).First(&user); result.Error != nil {
 		json.NewEncoder(w).Encode(e.ErrorResponse{404, "User tidak ditemukan"})
 	} else {
-		match := PasswordVerify(body["password1_user"].(string), user.Password1User)
-		if match {
-			json.NewEncoder(w).Encode(e.SuccesResponse{0, "Succes", user})
+		if user.IsActive == 1 {
+			match := PasswordVerify(body["password1_user"].(string), user.Password1User)
+			if match {
+				json.NewEncoder(w).Encode(e.SuccesResponse{0, "Succes", user})
+			} else {
+				json.NewEncoder(w).Encode(e.ErrorResponse{404, "Password tidak sesuai"})
+			}
 		} else {
-			json.NewEncoder(w).Encode(e.ErrorResponse{404, "Password tidak sesuai"})
+			json.NewEncoder(w).Encode(e.SuccesResponse{0, "Succes", user})
 		}
 	}
 }
@@ -53,9 +57,13 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	var user e.User
 	json.NewDecoder(r.Body).Decode(&user)
 	if result := d.DB.Table("user").Where("tlp_user = ?", user.TlpUser).First(&user); result.Error != nil {
-		d.DB.Table("user").Create(&user)
-		d.DB.Table("user").Where("tlp_user = ?", user.TlpUser).First(&user)
-		json.NewEncoder(w).Encode(e.SuccesResponse{0, "Succes", user})
+		if result := d.DB.Table("user").Where("email_user = ?", user.EmailUser).First(&user); result.Error != nil {
+			d.DB.Table("user").Create(&user)
+			d.DB.Table("user").Where("tlp_user = ?", user.TlpUser).First(&user)
+			json.NewEncoder(w).Encode(e.SuccesResponse{0, "Succes", user})
+		} else {
+			json.NewEncoder(w).Encode(e.ErrorResponse{404, "Email sudah terdaftar"})
+		}
 	} else {
 		json.NewEncoder(w).Encode(e.ErrorResponse{404, "No. Handphone sudah terdaftar"})
 	}
