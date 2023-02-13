eadb54b05718f1edb2cef17c6465cee4dfd8043a
iqbalberubah <iqbal.berubah@gmail.com>
2023-02-13 09:32:24 -0800

    update

--- entity/transaction.go
+++ entity/transaction.go
@@ -12,23 +12,27 @@ type Transaction struct {
 	TypeTrk          int    `json:"type_trk"`
 	StatusTransaksi  int    `json:"status_transaksi"`
 	JumlahTrx        string `json:"jumlah_trx"`
-	StatusPembayaran int    `json:"status_pembayaran"`
 	TrxType2         int    `json:"trx_type2"`
+	StatusPembayaran int    `json:"status_pembayaran"`
+	SellerId         int    `json:"seller_id"`
 }
 
 type TransactionHistory struct {
 	// gorm.Model
-	IdTransaksi     int    `json:"id_transaksi"`
-	Date            string `json:"date"`
-	Keterangan      string `json:"keterangan"`
-	Balance         string `json:"balance"`
-	NumberTrx       string `json:"number_trx"`
-	IdProduct       int    `json:"id_product"`
-	IdUser          int    `json:"id_user"`
-	TypeTrk         int    `json:"type_trk"`
-	StatusTransaksi int    `json:"status_transaksi"`
-	JumlahTrx       string `json:"jumlah_trx"`
-	TypeProduct     string `json:"type_product"`
-	NmProduct       string `json:"nm_product"`
-	Foto            string `json:"foto"`
+	IdTransaksi      int    `json:"id_transaksi"`
+	Date             string `json:"date"`
+	Keterangan       string `json:"keterangan"`
+	Balance          string `json:"balance"`
+	NumberTrx        string `json:"number_trx"`
+	IdProduct        int    `json:"id_product"`
+	IdUser           int    `json:"id_user"`
+	TypeTrk          int    `json:"type_trk"`
+	StatusTransaksi  int    `json:"status_transaksi"`
+	JumlahTrx        string `json:"jumlah_trx"`
+	TrxType2         int    `json:"trx_type2"`
+	StatusPembayaran int    `json:"status_pembayaran"`
+	SellerId         int    `json:"seller_id"`
+	TypeProduct      string `json:"type_product"`
+	NmProduct        string `json:"nm_product"`
+	Foto             string `json:"foto"`
 }
